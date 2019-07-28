9c5af5880e397c8c49a6ba562fc97b0ece7c3f8c
niwat-krasu <52350369+niwat-krasu@users.noreply.github.com>
2019-07-28 09:43:51 +0700

    Add files via upload

---
+++ Exercise4_niwat_k.py
@@ -0,0 +1,9 @@
+FE=4
+GB=4
+IC=4
+CR=4
+print("-----your score-----")
+print("Foundation english:" ,FE)
+print("Geueral business:",GB)
+print("Inlroduclion to computer sgstems:",IC)
+print("Comguter programming:",CR)
